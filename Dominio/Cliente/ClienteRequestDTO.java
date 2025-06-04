0fd205ae39dc559abc783eb53ae7628355175d5c
ingres-dev <leonardo.maia.dev@gmail.com>
2025-06-04 19:50:51 -0300

    dados atualizados

--- Dominio/Cliente/ClienteRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package com.pagafacil.PagaFacil.Dominio.Cliente;
-
-public record ClienteRequestDTO(Long id, String cnpj,String nomeSocial,
-                                String email,String senha,int telefone,String endereco) {
-
-}
